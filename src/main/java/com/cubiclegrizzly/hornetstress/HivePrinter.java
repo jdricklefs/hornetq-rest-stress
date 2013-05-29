969b482823b238263b2659cd60924d2f85b3146d
John Ricklefs <jdricklefs@gmail.com>
2013-05-28 22:49:19 -0400

    Clean up inconsistent formatting.

--- src/main/java/com/cubiclegrizzly/hornetstress/HivePrinter.java
+++ src/main/java/com/cubiclegrizzly/hornetstress/HivePrinter.java
@@ -4,42 +4,37 @@ public class HivePrinter {
 
     private static final int DOTS_PER_LINE = 100;
 
-	private static int printCtr = 0;
-	
-	public static synchronized void printM()
-	{
-		printCtr+=1;
-		if(printCtr > DOTS_PER_LINE) {
-			System.out.print("\n");
-			printCtr = 0;
-		}
-		System.out.print("M");
-	}
+    private static int printCtr = 0;
 
-    private static String getPrefix(int id)
-    {
-        return "("+id+"): ";
+    public static synchronized void printM() {
+        printCtr += 1;
+        if (printCtr > DOTS_PER_LINE) {
+            System.out.print("\n");
+            printCtr = 0;
+        }
+        System.out.print("M");
     }
 
-    public static synchronized void printLn(String msg)
-    {
+    private static String getPrefix(int id) {
+        return "(" + id + "): ";
+    }
+
+    public static synchronized void printLn(String msg) {
         System.out.print("\n");
         System.out.println(msg);
         printCtr = 0;
     }
 
-    public static synchronized void printLn(int id, String msg)
-    {
+    public static synchronized void printLn(int id, String msg) {
         System.out.print("\n");
         System.out.println(getPrefix(id) + msg);
         printCtr = 0;
     }
 
-    public static synchronized void printErr(int id, String msg)
-    {
+    public static synchronized void printErr(int id, String msg) {
         System.out.print("\n");
         System.err.println(getPrefix(id) + msg);
         printCtr = 0;
     }
-	
+
 }
